0617b909b9756889487bae32470cad9c445037da
Andreas Maier <maier@Andreass-MacBook-Pro.local>
2017-02-20 14:42:29 +0100

    First python example. akm

---
+++ examples/fib.py
@@ -0,0 +1,15 @@
+import matplotlib.pyplot as plt
+
+
+def fib2(n):  # return Fibonacci series up to n
+    """Return a list containing the Fibonacci series up to n."""
+    result = []
+    a, b = 0, 1
+    while a < n:
+        result.append(a)    # see below
+        a, b = b, a+b
+    return result
+
+
+plt.plot(fib2(100))
+plt.show()
